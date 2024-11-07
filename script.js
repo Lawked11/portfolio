889fe105e0e5672527c0aa7a10955ef525f7979a
Keith Mabangue <keithmabangue11@gmail.com>
2024-11-07 11:20:00 +0800

    1st commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example script for potential future interactions
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Document is ready!");
+    // Additional JavaScript functionality can be added here
+});
